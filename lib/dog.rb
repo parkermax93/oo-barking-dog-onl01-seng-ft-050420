0d023b6b158e4ebee3ab8015c474da19ef0b2c74
Parker Williamson <parker.williamson@gmail.com>
2020-05-12 17:44:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,14 @@
 class Dog 
   
   def name=(dog_name)
-    @this_dogs_name = dog_name 
+    @name = dog_name 
   end
   
   def name
-    @this_dogs_name
+    @name
   end
-end
-
-class Dog 
-  def bark
-    puts "Woof!"
+  
+    def bark
+    puts "woof!"
   end
 end
